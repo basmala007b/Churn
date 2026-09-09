d6e49d2209f431922f0367e3804e6f2585215dae
basmala007b <bqapany@gmail.com>
2026-09-09 15:01:14 +0300

    Initial commit: churn prediction app

---
+++ app.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import pandas as pd
+import joblib
+
+# Load the trained model and encoders
+model = joblib.load('model.pkl')
+encoder = joblib.load('encoder.pkl')
+features = joblib.load('features.pkl')
+target_encoder = joblib.load('target_encoder.pkl')
+
+st.title("Customer Churn Prediction")
+
+columns = st.columns(3)
+
+tenure = columns[0].number_input("Tenure", min_value=0, max_value=500)
+
+TechSupport = columns[1].selectbox("TechSupport", ['No', 'Yes', 'No internet service'])
+
+Contract = columns[2].selectbox("Contract", ['Month-to-month', 'One year', 'Two year'])
+
+df = pd.DataFrame([[tenure, TechSupport, Contract]], columns=features)
+
+if st.button('predict'):
+
+    for encode in encoder.keys():
+        df[encode] = encoder[encode].transform(df[encode])
+
+    predict = model.predict(df[features])
+    result = target_encoder.inverse_transform(predict)
+
+    st.write("prediction is: ", result[0])
